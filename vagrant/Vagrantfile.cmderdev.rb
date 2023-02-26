0356cd74715b8ad2bc3129f32019953e00d7710f
Dax T. Games <dtgames@kinggeek.org>
2023-02-25 20:14:18 -0500

    rename

--- vagrant/Vagrantfile.cmderdev.rb
+++ vagrant/Vagrantfile.cmderdev.rb
@@ -8,6 +8,7 @@ $env:path = "$env:path;c:\\tools\\cmder\\vendor\\git-for-windows\\cmd"
 c:
 cd \\Users\\Vagrant
 git clone https://github.com/cmderdev/cmder cmderdev
+TAKEOWN /F c:\\Users\\vagrant\\cmderdev /R /D y /s localhost /u vagrant /p vagrant
 cd cmderdev
 git remote add upstream  https://github.com/cmderdev/cmder
 git pull upstream master
@@ -34,6 +35,31 @@ Vagrant.configure("2") do |config|
     end
   end
 
+  config.vm.define "cmderdev-10-pro" do |cmderdev|
+    cmderdev.vm.hostname = 'cmderdev-10-pro'
+    cmderdev.vm.box = "cmderdev_win10_pro"
+
+    cmderdev.vm.provider :virtualbox do |v|
+      # v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      v.customize ["modifyvm", :id, "--name", "cmderdev-pro"]
+      v.customize ["modifyvm", :id, "--graphicscontroller", "vboxsvga"]
+      v.customize ["modifyvm", :id, "--memory", 8192]
+      v.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+    end
+  end
+  config.vm.define "cmderdev-10-pro-scaled" do |cmderdev|
+    cmderdev.vm.hostname = 'cmderdev-10-pro'
+    cmderdev.vm.box = "cmderdev_win10_pro"
+
+    cmderdev.vm.provider :virtualbox do |v|
+      # v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      v.customize ["modifyvm", :id, "--name", "cmderdev-pro"]
+      v.customize ["modifyvm", :id, "--graphicscontroller", "vboxsvga"]
+      v.customize ["modifyvm", :id, "--memory", 8192]
+      v.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
+      v.customize ["setextradata", :id, "GUI/ScaleFactor", "1.75"]
+    end
+  end
   config.vm.provision "shell", inline: $script_cmder
   config.vm.provision "shell", inline: $script_cmderdev
 end
