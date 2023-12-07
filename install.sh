fd448d7ed36c027e0ef1dcb2fb3be0b64de8d08d
Dax T. Games <dtgames@kinggeek.org>
2023-12-07 02:29:32 +0000

    Add support to install on Ubuntu

--- install.sh
+++ install.sh
@@ -5,6 +5,19 @@ if [ ! -d "$HOME/.yadr" ]; then
     git clone -b main --depth=1 https://github.com/daxgames/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
+
+    PLATFORM=$(uname)
+    if [ "${PLATFORM}" = "Linux" ] ; then
+        PLATFORM_FAMILY=$(lsb_release -is | tr [A-Z] [a-z])
+        PLATFORM_VERSION=$(lsb_release -ir)
+
+        if [ "${PLATFORM_FAMILY}" = "ubuntu" ] ; then
+            sudo apt install -y rake zsh
+        fi
+    elif [ "${PLATFORM}" = "Darwin" ] ; then
+        PLATFORM_FAMILY=$(echo ${PLATFORM} | tr [A-Z] [a-z])
+    fi
+    env | grep PLATFORM_
     rake install
 else
     echo "YADR is already installed"
