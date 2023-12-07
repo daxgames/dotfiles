7683513c062c79ff888d6b330dea98c1079ad9e3
Dax T. Games <dtgames@kinggeek.org>
2023-12-07 04:28:39 +0000

    Add support to install on rhel

--- install.sh
+++ install.sh
@@ -6,15 +6,27 @@ if [ ! -d "$HOME/.yadr" ]; then
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
 
-    PLATFORM=$(uname)
+    OS=$(uname)
     if [ "${PLATFORM}" = "Linux" ] ; then
-        PLATFORM_FAMILY=$(lsb_release -is | tr [A-Z] [a-z])
-        PLATFORM_VERSION=$(lsb_release -ir)
+        PLATFORM_FAMILY=rhel
+        if [ $(command -v lsb_release) ] ; then
+            PLATFORM=$(lsb_release -is | tr [A-Z] [a-z])
+            PLATFORM_FAMILY=$(lsb_release -is | tr [A-Z] [a-z])
+            PLATFORM_VERSION=$(lsb_release -rs)
+
+            [ "${PLATFORM}" = "centos" ] && PLATFORM_FAMILY=rhel
+        elif [ -f /etc/redhat-release ] ; then
+            PLATFORM=redhat
+            PLATFORM_FAMILY=rhel
+        fi
 
         if [ "${PLATFORM_FAMILY}" = "ubuntu" ] ; then
             sudo apt install -y rake zsh pip
-            pip install pynvim
+        elif [ "${PLATFORM_FAMILY}" = "rhel" ] ; then
+            sudo yum install -y rake zsh pip
         fi
+
+        pip install pynvim
     elif [ "${PLATFORM}" = "Darwin" ] ; then
         PLATFORM_FAMILY=$(echo ${PLATFORM} | tr [A-Z] [a-z])
     fi
