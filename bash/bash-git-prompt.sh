d092f622cb65251bdb756115f037c4e3643573dd
Dax T. Games <daxtgames@maximus.com>
2024-01-11 21:40:18 +0000

    x

--- bash/bash-git-prompt.sh
+++ bash/bash-git-prompt.sh
@@ -1,4 +1,4 @@
-GIT_PROMPT_ONLY_IN_REPO=1
-GIT_PROMPT_THEME=Solarized_UserHost
-source $HOME/.bash-git-prompt/gitprompt.sh
+#GIT_PROMPT_ONLY_IN_REPO=1
+#GIT_PROMPT_THEME=Solarized_UserHost
+#source $HOME/.bash-git-prompt/gitprompt.sh
 
