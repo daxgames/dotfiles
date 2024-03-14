68484758d5af709021100dbf1ea75cf05b73d196
Dax T. Games <daxtgames@maximus.com>
2024-03-14 14:10:42 -0400

    keybindings

--- nvim/lua/settings/plugins-lsp-config.lua
+++ nvim/lua/settings/plugins-lsp-config.lua
@@ -1,14 +1,15 @@
 require("mason").setup({
   ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
   }
 })
 
 require("mason-lspconfig").setup({
+  automatic_installation = true,
   ensure_installed = {
     "lua_ls",
     "tsserver",
@@ -19,7 +20,9 @@ require("mason-lspconfig").setup({
 local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup {}
 lspconfig.tsserver.setup {}
+lspconfig.groovyls.setup {}
 
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', 'gf', vim.lsp.buf.format, {})
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
