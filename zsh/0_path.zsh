d7f3b9438a8649dc6f4984a721e41fa75033c0f1
Dax T. Games <daxtgames@maximus.com>
2024-03-20 11:48:52 -0400

    Add pathDeDup

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -14,10 +14,14 @@ pathAppend() {
   fi
 }
 
+# Remove duplicate entries from PATH:
+pathDeDup() {
+  PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
+}
+
 [[ "$OSTYPE" == linux* ]] && [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-# Remove duplicate entries from PATH:
-PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
+pathDeDup
 
 pathAppend "$HOME/.yadr/bin"
 pathAppend "$HOME/.yadr/bin/yadr"
