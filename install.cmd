4adce41dce570d4b9c93fc48a906a7ede0a7a084
Dax T. Games <dtgames@kinggeek.org>
2024-03-30 09:51:54 -0400

    sync main

---
+++ install.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+if not exist "%USERPROFILE%\.yadr" (
+    echo "Installing YADR for the first time"
+    git clone -b main --depth=1 https://github.com/daxgames/dotfiles.git "%USERPROFILE%\.yadr"
+    cd "%USERPROFILE%\.yadr"
+    if "%~1" equ "ask" ( set ASK="true")
+    rake install
+) else (
+    echo "YADR is already installed"
+)
