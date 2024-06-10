ca7eedf326835327720c465d7eb8f0f3e903f0f2
Dax T. Games <daxtgames@maximus.com>
2024-06-10 11:44:39 -0400

    sync with daxgames

--- iTerm2/bootstrap-iterm2.sh
+++ iTerm2/bootstrap-iterm2.sh
@@ -5,11 +5,11 @@ tic ~/.yadr/iTerm2/xterm-256color.terminfo
 
 # Specify the preferences directory
 if [[ -f "$HOME/.yadr.user/iTerm2/com.googlecode.iterm2.plist" ]]; then
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.yadr/iTerm2/"
-fi
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.yadr.user/iTerm2/"
 
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+  # Tell iTerm2 to use the custom preferences in the directory
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+fi
 
 # Add shortcut to Dock
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
