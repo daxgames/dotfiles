2b444ce6e1c95ee44fdb2687544f2d6d8160b09a
Dax T. Games <daxtgames@maximus.com>
2024-06-21 19:13:07 +0000

    fixes

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,6 +1,6 @@
 # Load any custom after code
 if [ -d $HOME/.zsh.after/ ]; then
-  for config_file in $(ls $HOME/.zsh.after/ 2>/dev/null) ; do
+  for config_file in $(ls $HOME/.zsh.after/*.zsh 2>/dev/null) ; do
     [[ -n "${__YADR_DEBUG}" ]] && echo "source $config_file"
     source $config_file
   done
