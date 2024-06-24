652a038cd449d574a85d5b85687527a644c3fe13
Dax T. Games <dtgames@kinggeek.org>
2024-06-24 17:19:20 -0400

    x

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -1,7 +1,7 @@
 # Load any user customizations prior to load
 #
 if [ -d $HOME/.zsh.before/ ]; then
-  for config_file in $($HOME/.zsh.before/*.zsh 2>/dev/null) ; do
+  for config_file in $(ls $HOME/.zsh.before/*.zsh 2>/dev/null) ; do
     [[ -n "${__YADR_DEBUG}" ]] && echo "source $config_file"
     source $config_file
   done
