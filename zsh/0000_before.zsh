5ab9f717516788d7abbe9d2297365d680bff3a1e
Dax T. Games <dtgames@kinggeek.org>
2024-07-01 12:04:25 -0400

    less errors

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -1,6 +1,6 @@
 # Load any user customizations prior to load
 #
-if [ -d $HOME/.zsh.before/ ]; then
+if [ -d $HOME/.zsh.before/ ] && [ -n "$(ls $HOME/.zsh.before/ | grep "\.zsh" 2>/dev/null)" ];  then
   for config_file ($HOME/.zsh.before/*.zsh) ; do
     [[ -n "${__YADR_DEBUG}" ]] && echo "source $config_file"
     source $config_file
--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,5 +1,5 @@
 # Load any custom after code
-if [ -d $HOME/.zsh.after/ ]; then
+if [ -d $HOME/.zsh.after/ ] && [ -n "$(ls $HOME/.zsh.after/ | grep "\.zsh" 2>/dev/null)" ];  then
   for config_file ($HOME/.zsh.after/*.zsh) ; do
     [[ -n "${__YADR_DEBUG}" ]] && echo "source $config_file"
     source $config_file
