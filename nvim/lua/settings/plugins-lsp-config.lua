2cdfc2eaab26cbfbc52ea5ba0ccb7b29ca4b2964
Dax T. Games <dtgames@kinggeek.org>
2024-07-14 18:18:43 +0000

    add ls and cleanup

--- nvim/lua/settings/plugins-lsp-config.lua
+++ nvim/lua/settings/plugins-lsp-config.lua
@@ -11,24 +11,40 @@ require("mason").setup({
 require("mason-lspconfig").setup({
   automatic_installation = true,
   ensure_installed = {
-    "lua_ls",
-    "tsserver",
+    "bashls",
     "gradle_ls",
     "groovyls",
-    "yamlls",
     "jinja_lsp",
+    "lua_ls",
+    "rubocop",
+    "ruby_lsp",
+    "tsserver",
+    "yamlls",
   }
 })
 
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.bashls.setup {}
+lspconfig.gradle_ls.setup {}
 lspconfig.groovyls.setup {}
-lspconfig.yamlls.setup {}
 lspconfig.jinja_lsp.setup {}
+lspconfig.lua_ls.setup {}
+lspconfig.ruby_lsp.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.yamlls.setup {}
 
 vim.keymap.set('n', 'gf', vim.lsp.buf.format, {})
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'sh',
+  callback = function()
+    vim.lsp.start({
+      name = 'bash-language-server',
+      cmd = { 'bash-language-server', 'start' },
+    })
+  end,
+})
