00d0252345b730cbb5775e6fcda21bd538efa00d
Dax T. Games <dtgames@kinggeek.org>
2024-07-27 16:34:37 -0400

    cleanup

--- bash/zzzz_after.sh
+++ bash/zzzz_after.sh
@@ -1,6 +1,6 @@
 # Load any custom after code
 if [ -d $HOME/.bash.after/ ]; then
-  for config_file in $(ls $HOME/.bash.after/*.sh 2>/dev/null) ; do
+  for config_file in $(ls $HOME/.bash.after/*.sh 2>/dev/null | grep "\.sh$") ; do
     [ -n "${__YADR_DEBUG}" ] && echo sourcing $config_file
     source $config_file
   done
