cb16a101ae7cbbd81077d06e2c0fb66b335e83c1
Dax T. Games <dtgames@kinggeek.org>
2024-07-27 16:34:37 -0400

    cleanup

--- bash/000_before.sh
+++ bash/000_before.sh
@@ -1,7 +1,7 @@
 # Load any user customizations prior to load
 #
 if [ -d $HOME/.bash.before/ ]; then
-  for config_file in $(ls $HOME/.bash.before/*.sh 2>/dev/null) ; do
+  for config_file in $(ls $HOME/.bash.before/*.sh 2>/dev/null | grep "\.sh$") ; do
     [ -n "${__YADR_DEBUG}" ] && echo sourcing $config_file
     source $config_file ;
   done
