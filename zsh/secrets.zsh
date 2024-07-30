058a59b50da9049a13b9ab72cf24033485d7355a
Dax T. Games <daxtgames@maximus.com>
2024-07-30 12:11:33 -0400

    x

--- zsh/secrets.zsh
+++ zsh/secrets.zsh
@@ -8,6 +8,16 @@ if [[ $(uname) == Linux ]]; then
         export $(echo -n "$pass" | gnome-keyring-daemon --unlock --replace --components=pkcs11,secrets,ssh)
         unset pass
     }
+
+    if [[ $- == *i* ]]; then  # Only run in interactive mode
+      if [[ -z "${XDG_CURRENT_DESKTOP}" ]]; then
+        if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
+          exec dbus-run-session -- $SHELL
+        fi
+
+        gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
+      fi
+    fi
 fi
 
 if [ -e ~/.secrets ]; then
