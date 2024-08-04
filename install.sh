ebe9f88dc904b90c0425fd560124a329210f58dd
Dax T. Games <dtgames@kinggeek.org>
2024-08-04 10:36:58 -0400

    cleanup

--- install.sh
+++ install.sh
@@ -18,19 +18,19 @@ if [ ! -d "$HOME/.yadr" ]; then
     echo git clone -b "${git_branch}" --depth=1 "${git_repo}" "$HOME/.yadr"
     git clone -b "${git_branch}" --depth=1 "${git_repo}" "$HOME/.yadr"
     cd "$HOME/.yadr" || exit 1
-    [ "$1" == "ask" ] && export ASK="true"
+    [ "$1" = "ask" ] && export ASK="true"
 
     OS=$(uname)
-    if [ "${OS}" == "Linux" ] ; then
+    if [ "${OS}" = "Linux" ] ; then
         if [ -f /etc/os-release ] ; then
             echo "Determining Linux OS using '/etc/os-release'..."
             PLATFORM=$(grep -i ^id= /etc/os-release | cut -d = -f2 | sed 's/"//g')
             PLATFORM_FAMILY=$(grep -i ^id_like= /etc/os-release | cut -d = -f2 | sed 's/"//g')
             PLATFORM_VERSION=$(grep -i ^version_id= /etc/os-release | cut -d = -f2 | sed 's/"//g')
 
-            [ "${PLATFORM}" == "centos" ] && PLATFORM_FAMILY=rhel
-            [ "${PLATFORM}" == "fedora" ] && PLATFORM_FAMILY=rhel
-            [ "${PLATFORM}" == "debian" ] && PLATFORM_FAMILY=debian
+            [ "${PLATFORM}" = "centos" ] && PLATFORM_FAMILY=rhel
+            [ "${PLATFORM}" = "fedora" ] && PLATFORM_FAMILY=rhel
+            [ "${PLATFORM}" = "debian" ] && PLATFORM_FAMILY=debian
         elif [ -f /etc/redhat-release ] ; then
             PLATFORM=redhat
             PLATFORM_FAMILY=rhel
@@ -41,14 +41,14 @@ if [ ! -d "$HOME/.yadr" ]; then
 
         if [ -z "$(command -v rake)" ] ; then
             echo "Installing 'rake' in '${PLATFORM_FAMILY}'..."
-            if [ "${PLATFORM_FAMILY}" == "arch" ] ; then
+            if [ "${PLATFORM_FAMILY}" = "arch" ] ; then
                 $(command -v sudo) pacman -Syu
                 $(command -v sudo) pacman -S ruby-rake zip
-            elif [ "${PLATFORM_FAMILY}" == "debian" ]; then
+            elif [ "${PLATFORM_FAMILY}" = "debian" ]; then
                 $(command -v sudo) apt-get update -y
                 $(command -v sudo) apt-get install -y rake ruby-dev zip
 
-            elif [ "${PLATFORM_FAMILY}" == "rhel" ] ; then
+            elif [ "${PLATFORM_FAMILY}" = "rhel" ] ; then
                 [ "${PLATFORM_VERSION}" -lt 8 ] && PACKAGE_MANAGER=yum
                 [ "${PLATFORM_VERSION}" -gt 7 ] && PACKAGE_MANAGER=dnf
                 $(command -v sudo) "${PACKAGE_MANAGER}" update -y
@@ -56,7 +56,7 @@ if [ ! -d "$HOME/.yadr" ]; then
                 $(command -v sudo) "${PACKAGE_MANAGER}" install -y rubygem-rake zip
             fi
         fi
-    elif [ "${PLATFORM}" == "Darwin" ] ; then
+    elif [ "${PLATFORM}" = "Darwin" ] ; then
         PLATFORM_FAMILY="$(echo "${PLATFORM}" | tr  '[:upper:]' '[:lower:]')"
     fi
 
@@ -72,16 +72,12 @@ if [ ! -d "$HOME/.yadr" ]; then
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     fi
 
-    # if [ -z "$(command -v sdkman)" ] ; then
-    #     curl -s "https://get.sdkman.io?rcupdate=false" | bash
-    #     source "$HOME/.sdkman/bin/sdkman-init.sh"
-    # fi
-
     rake install
 else
     echo "YADR is already installed"
-    pushd $HOME/.yadr >>/dev/null 2>&1
+    current_dir=$(pwd)
+    cd "$HOME/.yadr" >>/dev/null 2>&1 || exit 1
     git pull --rebase
     rake update
-    popd >>/dev/null 2>&1
+    cd "$current_dir" >>/dev/null 2>&1 || exit 1
 fi
