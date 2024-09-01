095ae40af8f5d7daccd35f2e310a0e272899f12d
Dax T. Games <dtgames@kinggeek.org>
2024-09-01 16:27:58 -0400

    Adding Windows compatibility

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+__YADR_SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+export __YADR_SCRIPT_DIR
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing daxgames's YADR for the first time"
@@ -21,7 +24,12 @@ if [ ! -d "$HOME/.yadr" ]; then
     [ "$1" = "ask" ] && export ASK="true"
 
     OS=$(uname)
-    if [ "${OS}" = "Linux" ] ; then
+    if [[ "${OS}" =~ (MSYS) ]] || [[ "${OS}" =~ (MINGW) ]]; then
+        PLATFORM=windows
+        PLATFORM_FAMILY=windows
+        export PLATFORM PLATFORM_FAMILY
+        [ -z "${__YADR_DEBUG}" ] && env | grep PLATFORM | sort
+    elif [ "${OS}" = "Linux" ] ; then
         if [ -f /etc/os-release ] ; then
             echo "Determining Linux OS using '/etc/os-release'..."
             PLATFORM=$(grep -i ^id= /etc/os-release | cut -d = -f2 | sed 's/"//g')
@@ -39,39 +47,55 @@ if [ ! -d "$HOME/.yadr" ]; then
         export PLATFORM PLATFORM_FAMILY PLATFORM_VERSION
         [ -z "${__YADR_DEBUG}" ] && env | grep PLATFORM | sort
 
-        if [ -z "$(command -v rake)" ] ; then
-            echo "Installing 'rake' in '${PLATFORM_FAMILY}'..."
-            if [ "${PLATFORM_FAMILY}" = "arch" ] ; then
-                $(command -v sudo) pacman -Syu
-                $(command -v sudo) pacman -S ruby-rake zip
-            elif [ "${PLATFORM_FAMILY}" = "debian" ]; then
-                $(command -v sudo) apt-get update -y
-                $(command -v sudo) apt-get install -y rake ruby-dev zip
-
-            elif [ "${PLATFORM_FAMILY}" = "rhel" ] ; then
-                [ "${PLATFORM_VERSION}" -lt 8 ] && PACKAGE_MANAGER=yum
-                [ "${PLATFORM_VERSION}" -gt 7 ] && PACKAGE_MANAGER=dnf
-                $(command -v sudo) "${PACKAGE_MANAGER}" update -y
-                $(command -v sudo) "${PACKAGE_MANAGER}" groups install -y "Development Tools"
-                $(command -v sudo) "${PACKAGE_MANAGER}" install -y rubygem-rake zip
-            fi
-        fi
-    elif [ "${PLATFORM}" = "Darwin" ] ; then
+    elif [ "${OS}" = "Darwin" ] ; then
         PLATFORM_FAMILY="$(echo "${PLATFORM}" | tr  '[:upper:]' '[:lower:]')"
     fi
 
+    if [ -z "$(command -v rake)" ] ; then
+        echo "Installing YADR Pre-Reqs in '${PLATFORM_FAMILY}'..."
+        if [ "${PLATFORM_FAMILY}" = "windows" ] ; then
+            if [ -f "${__YADR_SCRIPT_DIR}/bin/install-windows-pre.ps1" ]; then
+                echo "Running '${__YADR_SCRIPT_DIR}/bin/install-windows-pre.ps1'..."
+                powershell -File "${__YADR_SCRIPT_DIR}/bin/install-windows-pre.ps1"
+                # MSYS use iWindows native symlinks
+                MSYS=winsymlinks:nativestict
+                CYGWIN=winsymlinks:nativestrict
+                export MSYS CYGWIN
+            fi
+        elif [ "${PLATFORM_FAMILY}" = "arch" ] ; then
+            $(command -v sudo) pacman -Syu
+            $(command -v sudo) pacman -S ruby-rake zip
+        elif [ "${PLATFORM_FAMILY}" = "debian" ]; then
+            $(command -v sudo) apt-get update -y
+            $(command -v sudo) apt-get install -y rake ruby-dev zip
+
+        elif [ "${PLATFORM_FAMILY}" = "rhel" ] ; then
+            [ "${PLATFORM_VERSION}" -lt 8 ] && PACKAGE_MANAGER=yum
+            [ "${PLATFORM_VERSION}" -gt 7 ] && PACKAGE_MANAGER=dnf
+            $(command -v sudo) "${PACKAGE_MANAGER}" update -y
+            $(command -v sudo) "${PACKAGE_MANAGER}" groups install -y "Development Tools"
+            $(command -v sudo) "${PACKAGE_MANAGER}" install -y rubygem-rake zip
+        fi
+    fi
     # Enable persistent undo
     mkdir -p "$HOME/.vim/backups" > /dev/null 2>&1
     mkdir -p "$HOME/.share/nvim/backups" > /dev/null 2>&1
 
-    if [ -z "$(command -v nvm)" ] && [ -z "$(command -v npm)" ] ; then
-        echo "Installing 'Node Version Manager and Node'..."
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-        NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-        export NVM_DIR
-        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    if [ "${OS}" = "Linux" ] ; then
+        if [ -z "$(command -v nvm)" ] && [ -z "$(command -v npm)" ] ; then
+            echo "Installing 'Node Version Manager and Node'..."
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+            NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+            export NVM_DIR
+            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+        fi
     fi
 
+    # until rake is in the path loop
+    until [ -n "$(command -v rake)" ] ; do
+        echo "Waiting for rake to be in the path..."
+        sleep 5
+    done
     rake install
 else
     echo "YADR is already installed"
