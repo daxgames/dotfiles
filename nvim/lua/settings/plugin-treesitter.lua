dd08fb1aae2e94b9b985afc6928ed13c484dacf3
Dax T. Games <dtgames@kinggeek.org>
2026-01-12 13:18:29 -0500

    treesitter fixes

--- nvim/lua/settings/plugin-treesitter.lua
+++ nvim/lua/settings/plugin-treesitter.lua
@@ -1,7 +1,21 @@
-local config = require("nvim-treesitter.configs")
-config.setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "yaml", "json", "bash", "javascript", "ruby", "diff" },
+-- nvim-treesitter(master) config - legacy
+-- local config = require("nvim-treesitter.configs")
+-- config.setup({
+--   -- A list of parser names, or "all" (the five listed parsers should always be installed)
+--   ensure_installed = { "lua", "vim", "vimdoc", "yaml", "json", "bash", "javascript", "ruby", "diff" },
+--   auto_install = true,
+--   indent = {
+--     enable = true,
+--   },
+--   highlight = {
+--     enable = true,
+--   },
+-- })
+
+-- nvim-treesitter(main) install
+require('nvim-treesitter').setup({
+  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+  install_dir = vim.fn.stdpath('data') .. '/site',
   auto_install = true,
   indent = {
     enable = true,
@@ -10,3 +24,8 @@ config.setup({
     enable = true,
   },
 })
+
+require("nvim-treesitter").install({
+  "lua", "vim", "vimdoc", "yaml", "json", "bash", "javascript", "ruby", "diff"
+})
+
